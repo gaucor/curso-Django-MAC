1f93f66fe9211a7ee59418506712f6aae531fa7e
gonzalocorrea <gaucor@gmail.com>
2023-03-25 20:01:35 -0300

    agregue filtros

--- bibloteca/gestion/admin.py
+++ bibloteca/gestion/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Libro, Autor, Editorial
 # Register your models here.
+class Libroadmin(admin.ModelAdmin):
+    list_display = ("titulo", "autor", "editorial", "sinopsis", "isbn")
+    list_filter =  ("autor", "editorial")
+    ordering = ("titulo", "autor", "editorial", "sinopsis", "isbn")
+    search_fields = ("titulo", "sinopsis", "isbn")
 
-admin.site.register(Libro)
-admin.site.register(Autor)
-admin.site.register(Editorial)
+admin.site.register(Libro, Libroadmin)
+
+class AutorAdmin (admin.ModelAdmin):
+    list_display = ("nombre", "apellidos", "pseudonimo", "nacionalidad", "fechaNacimiento", "fechaFallecimiento", "foto")
+    search_fields = ("nombre", "apellidos", "pseudonimo", "nacionalidad")
+
+admin.site.register(Autor, AutorAdmin)
+
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "telefono")
+
+admin.site.register(Editorial, EditorialAdmin)
+
